6dbcba2dcec4ed31a07c73252df0c0a7c648bc58
mor <mornahum91@gmail.com>
2021-05-31 22:58:36 +0300

    complete login and registration

--- application/controllers/Calendar.php
+++ application/controllers/Calendar.php
@@ -6,9 +6,11 @@ class Calendar extends CI_Controller
 	public function __construct()
 	{
 		parent::__construct();
-		$this->load->model('Calender_model');
+		$this->load->model('Activity_model');
+                $this->load->model('Message_model');
+                $this->load->model('Calender_model');
 		$this->load->library('session');
-		$this->load->helper('form');
+                $this->load->helper('form');
 	}
 
 	public function calendar()
@@ -52,5 +54,63 @@ class Calendar extends CI_Controller
 
 		$this->Calender_model->update_meeting($data);
 	}
+
+        //By Mor
+        	public function do_meeting()
+	{
+		$data=array(
+			'booker_email' => $this->input->post('booker_email'), //or  seesion?
+                    	'guide_email' => $this->input->post('guide_email'),
+			'subject' => $this->input->post('subject'),
+			'date' => $this->input->post('date'),
+		);
+                
+		$error = $this->Calender_model->save_meeting($data);
+                if ($error) {
+			echo json_encode(array('error' => true,'db_error' => $error['message']));
+			return;
+		}
+ 
+                else
+                {
+		$data2=array(
+			'recipient_email' => $this->input->post('guide_email'),
+                    	'sent_from' => $this->input->post('booker_email'),
+			'subject' => "קביעת פגישה",
+                        'content' => $this->input->post('subject'),
+                    
+		);
+                $error= $this->Message_model->send($data2);
+                 if ($error) {
+			echo json_encode(array('error' => true,'db_error' => $error['message']));
+			return;
+		}
+                }
+                		echo json_encode(array('success' => true));
+
+	}
+        
+        //By mor
+           public function approve_meeting()
+        {
+               $id = $this->input->post('id');
+               $newdata=array(
+			'booked' => $this->input->post('booked'),
+		);
+               
+               $error = $this->Calender_model->update($id, $newdata);
+		if ($error) {
+			$errors = array('error' => true,'db_error' => $error);
+			echo json_encode($errors);
+			return;
+		}
+
+		echo json_encode(array('success' => true));
+	}
+        
+  
+       
+        
+
 
 }
--- application/controllers/Guide.php
+++ application/controllers/Guide.php
@@ -1,39 +1,69 @@
 <?php
 
+class Guide extends CI_Controller {
 
-class Guide extends CI_Controller
-{
+    public function __construct() {
+        parent::__construct();
+        $this->load->model('Guide_model');
+        $this->load->model('Parents_model');
+        $this->load->library('session');
+    }
 
-	public function __construct()
-	{
-		parent::__construct();
-		$this->load->model('Guide_model');
-		$this->load->library('session');
-	}
+    public function add_expanse() {
+        $guide_email = $this->session->users_email;
+        $data = array(
+            $this->input->post($guide_email),
+            'edate' => $this->input->post('edate'),
+            'ename' => $this->input->post('ename'),
+            'price' => $this->input->post('price'),
+            'pic' => $this->input->post('pic'), //??
+            '$guide_email' => $this->input->post($guide_email),
+            'description' => $this->input->post('description')
+        );
+        $this->Guide_model->save_expense($data);
+    }
 
-	public function add_expanse()
-	{
-		$guide_email=$this->session->users_email;
-		$data=array(
-			$this->input->post($guide_email),
-			'edate' => $this->input->post('edate'),
-			'ename' => $this->input->post('ename'),
-			'price' => $this->input->post('price'),
-			'pic' => $this->input->post('pic'), //??
-			'$guide_email' => $this->input->post($guide_email),
-			'description' => $this->input->post('description')
-		);
-		$this->Guide_model->save_expense($data);
-	}
+    public function expanse_report() {
+        $guide_email = $this->session->users_email;
+        $this->Guide_model->get_expanses_by_time_DESC($guide_email);
+    }
 
-	public function expanse_report()
-	{
-		$guide_email=$this->session->users_email;
-		$this->Guide_model->get_expanses_by_time_DESC($guide_email);
-	}
+    public function dashboard() {
+        $this->load->view('Instructor/homepage.php');
+    }
 
-	public function dashboard()
-	{
-		$this->load->view('Instructor/homepage.php');
-	}
-}
+    //By Mor
+    //return fname, lname, email for the Guides of his children
+    public function get_guides_by_parent_email() {
+        
+        $parent_email=$this->input->post('parent_email');
+        
+        //looking for his children 
+        $res = $this->Parents_model->get_children($parent_email);
+        //taking only there agegredes
+        $agegrades=array();
+        foreach ($res as &$child) {
+            $agegrades[$child->agegrade_id]= $child->agegrade_id;
+        }
+        //looking for guides deatls - fname, lname, and email;
+        $guides=array();
+            foreach ($agegrades as &$value) {
+                $res=$this->Guide_model->get_guides_by_agegrade_id($value);
+                foreach ($res as $i)
+                {
+                   $guides[]=array($i->fname,$i->lname,$i->email); 
+                }
+                
+            }
+                    //return json_encode($guides);
+                    echo json_encode($guides);
+         
+            
+        
+    
+        }
+        
+ 
+    }
+
+
